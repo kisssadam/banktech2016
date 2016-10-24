eb4a77358dd8e8e25db854c38a959140a01a8dee
Adam Kiss <kisssandoradam@users.noreply.github.com>
2016-10-24 08:52:57 +0200

    Question 8

---
+++ src/round1/Question8/src/Question8.java
@@ -0,0 +1,19 @@
+public class Question8 {
+
+//	public interface MyObject {
+//		
+//	}
+
+//	public enum MyObject {
+//		
+//	}
+	
+	public static class MyObject {
+		
+	}
+	
+	public static void main(String[] args) {
+		Object obj = new MyObject(){};
+	}
+	
+}
