a86fa33da2325201571eebe2829af8b2eb079fb2
ZsocaCoder <zotyarex@gmail.com>
2016-11-09 22:14:27 +0100

    Position hashCode() restore.

--- src/NPE/src/main/java/hu/javachallenge/torpedo/model/Position.java
+++ src/NPE/src/main/java/hu/javachallenge/torpedo/model/Position.java
@@ -37,14 +37,14 @@ public class Position {
 		this.y = y;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 11321;
-//		int result = 1;
-//		result = prime * result + ((x == null) ? 0 : x.hashCode());
-//		result = prime * result + ((y == null) ? 0 : y.hashCode());
-//		return result;
-//	}
+	@Override
+	public int hashCode() {
+		final int prime = 11321;
+		int result = 1;
+		result = prime * result + ((x == null) ? 0 : x.hashCode());
+		result = prime * result + ((y == null) ? 0 : y.hashCode());
+		return result;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
