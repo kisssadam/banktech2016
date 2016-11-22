0500bf21496674e48a68a21038b8815173d52c8d
Adam Kiss <kisssandoradam@gmail.com>
2016-11-22 15:21:55 +0100

    serious bugfix

--- src/NPE/src/main/java/hu/javachallenge/torpedo/controller/GameController.java
+++ src/NPE/src/main/java/hu/javachallenge/torpedo/controller/GameController.java
@@ -379,7 +379,7 @@ public class GameController implements Runnable {
 						moveParameter.setSteering(moveParameterBasedOnTorpedos.getSteering());
 					}
 				}
-				if (moveParameter.getAcceleration() * (2.0 / 3.0) >= submarine.getVelocity()) {
+				if (maxAccelerationPerRound * (2.0 / 3.0) >= submarine.getVelocity()) {
 					moveParameter.setAcceleration(0.0);
 				}
 				callHandler.move(gameId, submarine.getId(), moveParameter.getAcceleration(), moveParameter.getSteering());
